97a0ef36295e3f699acb346f0f98be5c5d837ada
Harold Jimenez <hjimenez@expediagroup.com>
2024-08-15 22:14:57 -0500

    added parameter(s3_enable_inventory_tables) to enable/disable tables and cronjob

--- k8s-cronjobs.tf
+++ k8s-cronjobs.tf
@@ -69,6 +69,10 @@ resource "kubernetes_cron_job" "apiary_inventory" {
                 value = var.s3_enable_inventory
               }
               env {
+                name  = "ENABLE_S3_INVENTORY_TABLES"
+                value = var.s3_enable_inventory_tables
+              }
+              env {
                 name  = "APIARY_S3_INVENTORY_TABLE_FORMAT"
                 value = var.s3_inventory_format
               }
